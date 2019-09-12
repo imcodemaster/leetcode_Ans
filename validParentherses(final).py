010c7cf387db3deaf05eec5f76d0dd8d8d43129a
Vinay Bhatt <vinaybhatt0210@gmail.com>
2019-09-12 09:43:50 +0530

    Update validParentherses(final).py

--- validParentherses(final).py
+++ validParentherses(final).py
@@ -6,24 +6,25 @@ closelist = [')', '}', ']']
 def check (string):
     stack = []
     for i in string :
-        print('i in string : ' , i)
+        print('i in string : ' , i) #command just to check and understand what happen 
         if i in openlist :
-            print('i in openlist : ', i)
+            print('i in openlist : ', i) #command just to check and understand what happen
             stack.append(i)
-            print('append in stack : ' , stack)
+            print('append in stack : ' , stack) #command just to check and understand what happen
              
         elif i in closelist :
             pos = closelist.index(i)
-            print ('pos : ' , pos)
+            print ('pos : ' , pos) #command just to check and understand what happen
+            
             #lenght of stack after appending string is more then 0
             #and pos in openlist is strickly
             #equal to last element in stack (last in first out) 
             if len(stack) > 0 and openlist [pos] == stack[len(stack)-1] :
-                print('stack to check : ' , stack)
+                print('stack to check : ' , stack) #command just to check and understand what happen
 
                 print('last element : ' , stack[len(stack)-1])
                 stack.pop() #pop it 
-                print('stack after poping : ' , stack)
+                print('stack after poping : ' , stack) #command just to check and understand what happen
             else:
                 return 'Unbalanced'
         if len(stack) == 0 :
