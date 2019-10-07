fed4c33e592bfad5124e0df695e068822f94b5b2
Vinay Bhatt <vinaybhatt0210@gmail.com>
2019-10-07 06:42:58 +0530

    Update twopointer.py

--- twopointer.py
+++ twopointer.py
@@ -3,17 +3,14 @@ num = [2,7,11,15]
 n = len(num)
 x = 9
     
-class Solution ():
-    def twoSum (nums , x ):
-        i = 0
-        j = n - 1
-        while i <= j :
-            if num[i] + num[j] == x:
-                print (num[i] , num[j])
-                return True
-            elif num[i] + num[j] < x :
-                    i = i + 1
-            else :
-                    j = j - 1
-        return False
+class Solution(object):
+    def twoSum(self, nums, target):
+        if len(nums) <= 1:
+            return False
+        buff_dict = {}
+        for i in range(len(nums)):
+            if nums[i] in buff_dict:
+                return [buff_dict[nums[i]], i]
+            else:
+                buff_dict[target - nums[i]] = i
 print (Solution.twoSum(num , x))
